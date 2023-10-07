fcc670c304a7a4e0ca1a7c830775f361aba0c515
juan astudillo <juan.astudillomurua@gmail.com>
2023-10-07 01:50:54 -0300

    JAM 07_10_23

---
+++ BasicUI2/app/src/main/java/com/example/basicui2/batList.java
@@ -0,0 +1,14 @@
+package com.example.basicui2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class batList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bat_list);
+    }
+}
